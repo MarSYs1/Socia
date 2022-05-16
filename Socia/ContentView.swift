b95a4107c0798d76b8e246b9f281efa096131c6c
Mauro de Oliveira Andre <mauroolivere@hotmail.com>
2022-05-16 23:31:39 +0100

    Initial Commit

---
+++ Socia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Socia
+//
+//  Created by Mauro Sys One on 5/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Socia/SociaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SociaApp.swift
+//  Socia
+//
+//  Created by Mauro Sys One on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SociaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
